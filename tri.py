7bc7ccae86d6083ab4fbc4d03e3581892cb30e76
Hugola33290 <61471263+Hugola33290@users.noreply.github.com>
2021-03-02 09:22:05 +0100

    Add files via upload

--- tri.py
+++ tri.py
@@ -1,13 +1,19 @@
 def tri (tab):
 
-    i = ...
-    j = ...
+    i = 0
+    j = len(tab)-1
     while i != j:
         if tab[i] == 0:
-            i = ...
+            i = i + 1
+
         else:
             valeur = tab[j]
-            tab[j] = ...
-            ...
-            j = ...
-        ...
+            tab[j] = 1
+            tab[j] = tab[i]
+            tab[i] = valeur
+            j = j - 1
+
+    return tab
+
+tri([0,1,0,1,0,1,0,1,0])
+# doit retourner [0, 0, 0, 0, 0, 1, 1, 1, 1]
