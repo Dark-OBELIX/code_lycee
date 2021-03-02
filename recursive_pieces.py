747ac319cc00b70b93484869a7be66f604d135bd
Hugola33290 <61471263+Hugola33290@users.noreply.github.com>
2021-03-02 08:43:36 +0100

    Update recursive_pieces.py

--- recursive_pieces.py
+++ recursive_pieces.py
@@ -9,5 +9,5 @@ def rendu_pieces_recur(somme, S, pieces_rendues=[], ind=0):
     else:
         return rendu_pieces_recur(somme, S, pieces_rendues=[], ind=ind+1)
 
-S = [500, 200, 100, 50, 20, 10, 5, 2, 1]
-print(rendu_pieces_recur(7, S))
+S = [500, 200, 100, 50, 20, 10, 5, 2, 1] # pieces differentes disponibles
+print(rendu_pieces_recur(753, S)) # ici il faut rendre 753 euro avec le moins de pieces possibles
