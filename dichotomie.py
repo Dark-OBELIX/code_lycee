f8df8b6198463e338bfbbfa6b555e9208cdc08a5
Hugola33290 <61471263+Hugola33290@users.noreply.github.com>
2021-03-08 09:51:40 -0100

    Update dichotomie.py

--- dichotomie.py
+++ dichotomie.py
@@ -4,17 +4,17 @@ def dichotomie(tab, x):
     fin = len(tab) - 1
 
     while debut <= fin:
-        m = ...
+        m = (debut + fin) // 2
         if x == tab[m]:
-            return ...
+            return True
 
         if x > tab[m]:
             debut = m + 1
 
         else:
-            fin = ...
+            fin = m - 1
 
-    return ...
+    return False
 
 #dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33], 28)
 # True
