e928d295405692be44e0530a41f55b97d41f33c2
Hugola33290 <61471263+Hugola33290@users.noreply.github.com>
2021-03-16 07:24:03 -0100

    Update tri_insertion.py

--- tri_insertion.py
+++ tri_insertion.py
@@ -1,17 +1,21 @@
 def tri_insertion(L):
+
 	n = len(L)
-	if ...:
+	#cas d'un tableau vide
+	if n == 0:
 		return L
 
 	for j in range(1, n):
 		e = L[j]
 		i = j
 
-		while i > 0 and L[i-1] > ...:
-
+		while i > 0 and L[i-1] > L[j]:
+			i = i - 1
 		if i != j:
-			for k in range (j,i,...):
-				L[k] = L[...]
-			L[i] = ...
-		return L
-		
+			for k in range (j,i,-1):
+				L[k] = L[k - 1]
+	
+			L[i] = e
+	return L
+
+print(tri_insertion([10,9,8,7,6,5,4,3,2,1])) 
