deb1990fb030850563d69e9dd6d2e074616788e2
Hugola33290 <61471263+Hugola33290@users.noreply.github.com>
2021-03-16 06:26:14 -0100

    Add files via upload

---
+++ tri_insertion.py
@@ -0,0 +1,17 @@
+def tri_insertion(L):
+	n = len(L)
+	if ...:
+		return L
+
+	for j in range(1, n):
+		e = L[j]
+		i = j
+
+		while i > 0 and L[i-1] > ...:
+
+		if i != j:
+			for k in range (j,i,...):
+				L[k] = L[...]
+			L[i] = ...
+		return L
+		
