69f35f88e68fac906923952b39e694eaa955ae09
Hugola33290 <61471263+Hugola33290@users.noreply.github.com>
2021-03-26 06:08:15 -0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,23 @@
 def fibonacci(valeur):
-    lastval = 1
-    lastval2 = 1
-    newN=0
+	
+    var_last = 1
+    var_last_2 = 1
+    var_temp = 0
     i=0
 
     while i <= valeur:
         if i > 2:
-            newN = lastval+lastval2
-            lastval2 = lastval
-            lastval = newN
+            var_temp = var_last + var_last_2
+            var_last_2 = var_last
+            var_last = var_temp
         else:
-            newN = 1
-        i+=1
-    return newN
+            var_temp = 1
+        i = i + 1
+
+    return var_temp
+
+############################################################
+
+print(fibonacci(7))
+
+# 13
