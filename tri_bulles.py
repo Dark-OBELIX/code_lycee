5cf8c12210202576f6573ba1a60f00cf680c46c1
MMGFORCE <64776096+MMGFORCE@users.noreply.github.com>
2021-03-29 09:32:03 +0200

    Add files via upload

---
+++ tri_bulles.py
@@ -0,0 +1,12 @@
+def tri_bulles(T):
+    n = len(T)
+    for i in range(n-1):
+        for j in range(n-1):
+            if T[j] > T[j+1]:
+                temp = T[j]
+                T[j] = T[j+1]
+                T[j+1] = temp
+    return T
+
+T = [4, 2, 6, 5, 7]
+print(tri_bulles(T))
